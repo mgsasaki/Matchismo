b2f236b1e92ab1a4805053043435b6ff983a83ce
Marcello Sasaki <mgsasaki@ibest.com.br>
2013-03-04 23:47:58 -0300

    Homework #0 - Until Lecture 2

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,13 +7,25 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic) Deck * deck;
 @end
 
 @implementation CardGameViewController
+
+- (Deck *)deck
+{
+    if (!_deck) {
+        _deck = [[PlayingCardDeck alloc] init];
+    }
+    
+    return _deck;
+}
 
 - (void)setFlipCount: (int)flipCount
 {
@@ -24,7 +36,18 @@
 
 - (IBAction)flipCard:(UIButton *)sender
 {
-    sender.selected = !sender.isSelected;
+    if (!sender.isSelected)
+    {
+        Card * card = [self.deck drawRandomCard];
+        
+        [sender setTitle: card.contents forState: UIControlStateSelected];
+        
+        sender.selected = YES;
+    }
+    else
+    {
+        sender.selected = NO;
+    }
     self.flipCount++;
 }
 
